3885405ce951e1a39a62ba4bd1b20e40bf330572
Aspiring Astro <104707961+aspiringastro@users.noreply.github.com>
2023-01-05 10:26:27 -0800

    refactor nbaction

--- src/nbaction.py
+++ src/nbaction.py
@@ -17,12 +17,13 @@ def publish_notebooks(src_path, doc_path):
     subprocess.run(cmd)
 
 repo_workspace = os.environ.get("GITHUB_WORKSPACE", '.')
-src_path = os.path.join(repo_workspace, os.environ.get("NB_SOURCE_PATH", 'src'))
+sources = os.path.join(repo_workspace, os.environ.get("NB_SOURCES"))
 target_path = os.path.join(repo_workspace, os.environ.get("NB_TARGET_PATH", 'target/note'))
 doc_path = os.path.join(repo_workspace, os.environ.get("NB_DOC_PATH", 'target/doc'))
 
+all_sources = set([ os.path.join(repo_workspace, os.path.dirname(f)) for f in sources ])
+processed = []
 
-all_sources = set([ os.path.dirname(f)[len(repo_workspace)+1:] for f in glob.glob(f'{src_path}/**/*.ipynb', recursive=True) ])
 for s in all_sources:
     if '.ipynb_checkpoints' in s:
         continue
@@ -31,9 +32,14 @@ for s in all_sources:
     di = os.path.join(repo_workspace, doc_path)
     os.makedirs(ti, exist_ok=True)
     os.makedirs(di, exist_ok=True)
+
     clean_notebooks(si)
     exec_notebooks(si, ti)
     publish_notebooks(os.path.join(ti, "*.ipynb"), di)
+    for sif in os.walk(si): processed.add(sif)
+    for tif in os.walk(os.path.join(ti,"*.ipynb")): processed.add(sif)
+    for dif in os.walk(os.path.join(di,"*.html")): processed.add(dif)
 
+print("processed=", ' '.join(map(str, processed)))
 
 
