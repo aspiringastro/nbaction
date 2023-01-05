924f76501db530e570f5feee830930a1397c156a
Aspiring Astro <104707961+aspiringastro@users.noreply.github.com>
2023-01-05 11:26:06 -0800

    filter for ipynb

--- src/nbaction.py
+++ src/nbaction.py
@@ -5,6 +5,9 @@ import glob
 import subprocess
 from pathlib import Path
 
+NOTEBOOK_EXTN = ".pynb"
+HTML_EXTN = ".html"
+
 def exec_notebook(src, target):
     cmd = ["jupyter", "nbconvert", "--ExecutePreprocessor.timeout=600", "--to",  "notebook", "--execute", "--output=" + target, src ]
     subprocess.run(cmd)
@@ -28,24 +31,25 @@ processed = []
 for s in all_sources:
     if '.ipynb_checkpoints' in s:
         continue
-    f = Path(s).stem
     basename = os.path.basename(s)
-    sif = os.path.join(repo_workspace, s)
-    ti = os.path.join(repo_workspace, target_path)
-    tif = os.path.join(ti, basename)
-    di = os.path.join(repo_workspace, doc_path)
-    dif = os.path.join(di, f + ".html")
+    f, extn = os.path.splitext(basename)
+    if extn.lower() == NOTEBOOK_EXTN:
+        sif = os.path.join(repo_workspace, s)
+        ti = os.path.join(repo_workspace, target_path)
+        tif = os.path.join(ti, basename)
+        di = os.path.join(repo_workspace, doc_path)
+        dif = os.path.join(di, f + HTML_EXTN)
 
-    os.makedirs(ti, exist_ok=True)
-    os.makedirs(di, exist_ok=True)
+        os.makedirs(ti, exist_ok=True)
+        os.makedirs(di, exist_ok=True)
 
-    clean_notebook(sif)
-    exec_notebook(sif, tif)
-    publish_notebook(tif, dif)
+        clean_notebook(sif)
+        exec_notebook(sif, tif)
+        publish_notebook(tif, dif)
 
-    processed.add(sif)
-    processed.add(tif)
-    processed.add(dif)
+        processed.add(sif)
+        processed.add(tif)
+        processed.add(dif)
 
 print("processed=", ' '.join(map(str, processed)))
 
