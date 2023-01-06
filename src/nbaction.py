032a761475149e9b15b293562960b29d026830db
Aspiring Astro <104707961+aspiringastro@users.noreply.github.com>
2023-01-05 17:14:22 -0800

    clean up

--- src/nbaction.py
+++ src/nbaction.py
@@ -9,20 +9,21 @@ HTML_EXTN = ".html"
 
 def exec_notebook(src, target):
     cmd = ["jupyter", "nbconvert", "--ExecutePreprocessor.timeout=600", "--to",  "notebook", "--execute", "--output=" + target, src ]
-    subprocess.run(cmd)
+    return subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
 def clean_notebook(src):
     cmd = ["jupyter", "nbconvert", "--ExecutePreprocessor.timeout=600", "--clear-output", src ]
-    subprocess.run(cmd)
+    return subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
 def publish_notebook(src, target):
     cmd = ["jupyter", "nbconvert", "--ExecutePreprocessor.timeout=600", "--to", "html", "--output=" + target,  src ]
-    subprocess.run(cmd)
+    return subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
+# Main
 repo_workspace = os.environ.get("GITHUB_WORKSPACE", '.')
 sources = os.environ.get("NB_SOURCES", '').split(' ')
-target_path = os.path.join(repo_workspace, os.environ.get("NB_TARGET_PATH", 'target/note'))
-doc_path = os.path.join(repo_workspace, os.environ.get("NB_DOC_PATH", 'target/doc'))
+target_path = os.path.join(repo_workspace, os.environ.get("NB_TARGET_PATH", 'publish/notebook'))
+doc_path = os.path.join(repo_workspace, os.environ.get("NB_DOC_PATH", 'publish/doc'))
 
 all_sources = set([ os.path.join(repo_workspace, f) for f in sources ])
 processed = []
@@ -33,27 +34,35 @@ for s in all_sources:
     basename = os.path.basename(s)
     f, extn = os.path.splitext(basename)
 
-    if extn.lower() == NOTEBOOK_EXTN:
-        ti = os.path.join(repo_workspace, target_path)
-        tif = os.path.join(ti, basename)
-        di = os.path.join(repo_workspace, doc_path)
-        dif = os.path.join(di, f + HTML_EXTN)
+    if extn.lower() != NOTEBOOK_EXTN:
+        continue
 
-        os.makedirs(ti, exist_ok=True)
-        os.makedirs(di, exist_ok=True)
+    ti = os.path.join(repo_workspace, target_path, os.path.dirname(s))
+    tif = os.path.join(ti, basename)
+    di = os.path.join(repo_workspace, doc_path, os.path.dirname(s))
+    dif = os.path.join(di, f + HTML_EXTN)
 
-        clean_notebook(s)
-        exec_notebook(s, tif)
-        publish_notebook(tif, dif)
+    os.makedirs(ti, exist_ok=True)
+    os.makedirs(di, exist_ok=True)
 
-        processed.append(s)
-        processed.append(tif)
-        processed.append(dif)
+    result = clean_notebook(s)
+    if result.returncode != 0:
+        print(f"ERROR: clean_notebook {basename} failed with code {result.returncode}")
+    result = exec_notebook(s, tif)
+    if result.returncode != 0:
+        print(f"ERROR: exec_notebook {basename} -> {tif} failed with code {result.returncode}")
+    result = publish_notebook(tif, dif)
+    if result.returncode != 0:
+        print(f"ERROR: publish_notebook {basename} -> {dif} failed with code {result.returncode}")
 
+    processed.append(s)
+    processed.append(tif)
+    processed.append(dif)
+
+print("{0}={1}".format("published", ' '.join(processed)), file=sys.stdout)
 if "GITHUB_OUTPUT" in os.environ :
     with open(os.environ["GITHUB_OUTPUT"], "a") as f :
-        print("{0}={1}".format("processed", ' '.join(processed)), file=f)
-        print("{0}={1}".format("processed", ' '.join(processed)), file=sys.stdout)
+        print("{0}={1}".format("published", ' '.join(processed)), file=f)
 
 
 
